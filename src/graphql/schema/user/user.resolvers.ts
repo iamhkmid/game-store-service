ccf75739f806526557162c4627059fe459ae57ce
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-07-20 15:29:38 +0700

    init

--- src/graphql/schema/user/user.resolvers.ts
+++ src/graphql/schema/user/user.resolvers.ts
@@ -1,11 +1,12 @@
-// import { QueryResolvers } from "../../../types/graphql"
+import { QueryResolvers } from "../../../types/graphql"
 
-// export const Query: QueryResolvers = {
-//   user: async () => {
-//     return {}
-//   },
-//   users: async (_, __, { db }) => {
-//     const find = await db.user.findMany()
-//     return find.map((val) => ({ id: val.id, name: val.name, username: val.username, updatedAt: val.updatedAt, createdAt: val.createdAt, role: val.role  }))
-//   }
-// }
+export const Query: QueryResolvers = {
+  user: async () => {
+    return {}
+  },
+  users: async (_, __, { db }) => {
+    const find = await db.user.findMany()
+    console.log(find)
+    return []
+  }
+}
