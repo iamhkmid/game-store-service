abfb03a3483dfec46a7b78aa39664369c425b8a6
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-07-21 15:13:29 +0700

    fix start script

--- src/routes/altairRoute.ts
+++ src/routes/altairRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { altairEnv, altairMiddleware } from "../middlewares/altair";
+import { altairMiddleware } from "../middlewares/altair";
 
 export const altairRoute = Router()
 
-altairRoute.use("/", altairEnv, altairMiddleware)
+altairRoute.use("/", altairMiddleware)
